b3c6bfbc4b9478c420ad4643a79934e52b802ee7
AjeR <bdlugosz98@gmail.com>
2020-07-11 22:27:54 +0200

    Update

---
+++ web/routes.py
@@ -0,0 +1,32 @@
+import io
+import random
+from flask import render_template, url_for, flash, redirect, Response
+from web import app
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
+def create_figure():
+    fig = Figure()
+    axis = fig.add_subplot(1, 1, 1)
+    xs = range(100)
+    ys = [random.randint(1, 50) for x in xs]
+    axis.plot(xs, ys)
+    return fig
+
+@app.route("/")
+@app.route("/home")
+def home():
+    return render_template('home.html')
+
+
+@app.route("/about")
+def about():
+    return render_template('about.html', title='About')
+
+
+@app.route('/plot.png')
+def plot_png():
+    fig = create_figure()
+    output = io.BytesIO()
+    FigureCanvas(fig).print_png(output)
+    return Response(output.getvalue(), mimetype='image/png')
