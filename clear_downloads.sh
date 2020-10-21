0b9049767ccffa4938e3f92c2a68eacc5f7214a7
4jeR <bdlugosz98@gmail.com>
2020-10-21 11:59:51 +0200

    Models rebuilt, works for every model correctly. Options only for matplotlib.

--- clear_downloads.sh
+++ clear_downloads.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 cd web/downloads/
-rm -rf *
-mkdir codes
-mkdir images
+    rm -rf *
+    mkdir codes
+
+    cd codes
+        touch .gitkeep
+        cd ..
+    mkdir images
+    cd images
+        touch .gitkeep
+        cd ..
+    cd ../../
 exit
