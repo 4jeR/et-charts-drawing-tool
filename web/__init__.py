f72da2da9bf6198b60e70ffc378dfbd26f41ddae
4jeR <bdlugosz98@gmail.com>
2021-01-03 16:36:39 +0100

    Update

--- web/__init__.py
+++ web/__init__.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-# from flask_caching import Cache
 import os 
 
 CHARTS_FOLDER = os.path.join('static', 'plots')
@@ -9,11 +8,8 @@ app = Flask(__name__, static_folder='static')
 
 app.config['UPLOAD_FOLDER'] = CHARTS_FOLDER
 app.config['SECRET_KEY'] = 'c8ba4ab3f643089aed4ff7a6205e4815'
-# app.config['CACHE_TYPE'] = 'simple'
-# app.config['CACHE_DEFAULT_TIMEOUT'] = 300
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres:///et_charts'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
-# cache = Cache(app)
 from web import routes 
 from web import help_utils
